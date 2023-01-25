708ff576ddf9926a7891b56bcef17d52c8cd2775
Lucas Fernandes <lucas.fernandes1454@gmail.com>
2023-01-24 21:57:05 -0300

    Add Coverlet, test project and initial UserRepository methods

---
+++ SocialNetwork.Data/Repositories/Implementations/UserRepository.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SocialNetwork.Data.Context;
+using SocialNetwork.Data.Entities;
+
+namespace SocialNetwork.Data.Repositories.Implementations
+{
+    public class UserRepository
+    {
+        private readonly SocialNetworkContext _context;
+        public UserRepository(SocialNetworkContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<User?> GetUserByIdAsync(Guid userId)
+        {
+            User? user = await _context.Set<User>().FirstOrDefaultAsync(u => u.Id == userId);
+            return user;
+        }
+
+        public async Task<IEnumerable<User>?> GetUsersByNameAsync(string userName)
+        {
+            var users = await _context.Users?
+                .Where(u => u.FirstName.Contains(userName) || u.LastName.Contains(userName))
+                .ToListAsync()!;
+
+            return users;
+        }
+    }
+}
